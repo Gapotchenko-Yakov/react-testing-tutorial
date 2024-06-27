ec045876c287714fd8bf788dae93ef30ced7af21
Yakov-Gapotchenko <yakov.gapotchenko@gmail.com>
2024-06-27 21:49:29 +0300

    Test Driven Development at Greet component.

--- src/components/greet/greet.test.tsx
+++ src/components/greet/greet.test.tsx
@@ -6,3 +6,9 @@ test("Greet renders correctly", () => {
   const textElement = screen.getByText(/hello/i);
   expect(textElement).toBeInTheDocument();
 });
+
+test("Greet renders with a name", () => {
+  render(<Greet name="Yakov" />);
+  const textElement = screen.getByText("Hello, Yakov");
+  expect(textElement).toBeInTheDocument();
+});
