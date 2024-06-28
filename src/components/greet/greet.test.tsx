30fd854ed965909bbc546b20f7420a988c6ba8d3
Yakov-Gapotchenko <yakov.gapotchenko@gmail.com>
2024-06-28 11:55:40 +0300

    Grouping Tests

--- src/components/greet/greet.test.tsx
+++ src/components/greet/greet.test.tsx
@@ -1,14 +1,18 @@
 import { render, screen } from "@testing-library/react";
 import Greet from "./greet";
 
-test("Greet renders correctly", () => {
-  render(<Greet />);
-  const textElement = screen.getByText(/hello/i);
-  expect(textElement).toBeInTheDocument();
-});
+describe("Greet", () => {
+  test("renders correctly", () => {
+    render(<Greet />);
+    const textElement = screen.getByText(/hello/i);
+    expect(textElement).toBeInTheDocument();
+  });
 
-test.only("Greet renders with a name", () => {
-  render(<Greet name="Yakov" />);
-  const textElement = screen.getByText("Hello, Yakov");
-  expect(textElement).toBeInTheDocument();
+  describe.skip("Nested", () => {
+    test("renders with a name", () => {
+      render(<Greet name="Yakov" />);
+      const textElement = screen.getByText("Hello, Yakov");
+      expect(textElement).toBeInTheDocument();
+    });
+  });
 });
