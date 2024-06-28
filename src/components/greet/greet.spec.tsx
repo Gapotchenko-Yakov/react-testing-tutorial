12b23768bdbb08eee78d77ad4ddc3c9117c82052
Yakov-Gapotchenko <yakov.gapotchenko@gmail.com>
2024-06-28 12:37:17 +0300

    Filename Conventions and test func alternative.

--- src/components/greet/greet.spec.tsx
+++ src/components/greet/greet.spec.tsx
@@ -2,14 +2,14 @@ import { render, screen } from "@testing-library/react";
 import Greet from "./greet";
 
 describe("Greet", () => {
-  test("renders correctly", () => {
+  xit("renders correctly", () => {
     render(<Greet />);
     const textElement = screen.getByText(/hello/i);
     expect(textElement).toBeInTheDocument();
   });
 
   describe("Nested", () => {
-    test("renders with a name", () => {
+    it("renders with a name", () => {
       render(<Greet name="Yakov" />);
       const textElement = screen.getByText("Hello, Yakov");
       expect(textElement).toBeInTheDocument();
